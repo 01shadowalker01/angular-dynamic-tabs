8a94ac7c7df6f859bcc4dfc446c0178bd22ff095
shadowalker <01shadowalker01@gmail.com>
2020-11-12 00:31:25 +0330

    dynamic component implemented

--- src/app/tabs/tab-content.component.ts
+++ src/app/tabs/tab-content.component.ts
@@ -1,11 +1,38 @@
-import { Component, OnInit } from "@angular/core";
+import {
+  Component,
+  ComponentFactoryResolver,
+  Input,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
+import { TabContentHostDirective } from "./tab-content-host.directive";
+import { Tab } from "./tab";
 
 @Component({
   selector: "shw-tab-content",
-  template: "<ng-template></ng-template>"
+  template: "<ng-template tabContentHost></ng-template>",
 })
 export class TabContentComponent implements OnInit {
-  constructor() {}
+  @ViewChild(TabContentHostDirective, { static: true })
+  tabHost: TabContentHostDirective;
 
-  ngOnInit(): void {}
+  @Input() tabConfig: Tab;
+
+  constructor(private componentFactory: ComponentFactoryResolver) {}
+
+  ngOnInit() {
+    this.loadComponent();
+  }
+
+  loadComponent() {
+    const componentFactory = this.componentFactory.resolveComponentFactory(
+      this.tabConfig.component
+    );
+
+    const viewContainerRef = this.tabHost.viewContainerRef;
+    viewContainerRef.clear();
+
+    const componentRef = viewContainerRef.createComponent(componentFactory);
+    componentRef.instance.data = this.tabConfig.data;
+  }
 }
