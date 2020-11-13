e63105a12df00d8c57e5df2d882c624bd8c44a1a
shadowalker <01shadowalker01@gmail.com>
2020-11-13 22:08:35 +0330

    tabs config fixed. closable added

--- src/app/tabs/tabs.config.ts
+++ src/app/tabs/tabs.config.ts
@@ -6,11 +6,13 @@ export const TABS: Tab[] = [
   {
     title: "Avatars",
     component: AvatarListComponent,
+    closable: false,
     data: null
   },
   {
     title: "Search List",
     component: SearchListComponent,
+    closable: true,
     data: null
   }
 ];
